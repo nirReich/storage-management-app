90609b3501b85306952866be8c8885cf39bb1989
nir <reich.nir@gmail.com>
2020-08-07 15:45:19 +0300

    commiting build to start gh pages site

---
+++ build/precache-manifest.67b2252379fd3524ccc71e60a168c3c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46159294e58305d8c0accb9e7287d7a5",
+    "url": "/%3Cyour%20repository%20name%3E/index.html"
+  },
+  {
+    "revision": "b4f11e4dafb89ce34640",
+    "url": "/%3Cyour%20repository%20name%3E/static/css/main.d6d38e73.chunk.css"
+  },
+  {
+    "revision": "14692f5c8c8d292a9d60",
+    "url": "/%3Cyour%20repository%20name%3E/static/js/2.36314841.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%3Cyour%20repository%20name%3E/static/js/2.36314841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f11e4dafb89ce34640",
+    "url": "/%3Cyour%20repository%20name%3E/static/js/main.d0e26b0c.chunk.js"
+  },
+  {
+    "revision": "285667dea38db544eb46",
+    "url": "/%3Cyour%20repository%20name%3E/static/js/runtime-main.23fb8493.js"
+  }
+]);
